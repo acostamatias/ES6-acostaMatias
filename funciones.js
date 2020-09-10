8a193f558335fa3bba553f6febd636893ce323d9
acostamatias <69093336+acostamatias@users.noreply.github.com>
2020-09-10 18:00:19 -0300

    Ejercicios resueltos

---
+++ funciones.js
@@ -0,0 +1,19 @@
+const saludar = (nombre) => `Hola ${nombre}`;
+
+// Mostrar el resultado por consola
+console.log(saludar('Alejandro'))
+
+const multiplicar = (a, b) => a * b
+
+// Mostrar el resultado por consola
+console.log(multiplicar(3, 15))
+
+const crearAlumno = (nombre, apellido, edad, materias) => ({nombre,apellido,edad,materias})
+
+// Mostrar el resultado por consola.
+console.log(crearAlumno('Leo', 'Messi', '32', ['TLPA','Base de Datos','Inglés']))
+
+const devolverFecha = (...arguments) => `Hoy es ${arguments}`;
+
+// Mostrar el resultado por consola.
+console.log(devolverFecha('10','09','2020'))
